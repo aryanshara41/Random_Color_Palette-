b987eee7c78faf2cbd6c65f303c492553cfad0f1
Shubham Kumar <aryanshara41@gmail.com>
2023-02-11 21:59:18 +0530

    Second commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Generate() {
     main.innerHTML = "";
 
     var list = document.querySelector(".list");
-    
+
     for (let i = 0; i < 12; i++) {
         var hexcode = "#";
         var val = Math.floor(Math.random() * 0xffffff).toString(16);
@@ -15,8 +15,17 @@ function Generate() {
         ele.classList.add('list');
 
         ele.innerHTML = `<div class="container" style = "background-color: ${hexcode};"></div>
-        <span>${hexcode}</span>`;
+        <span class = "hex" id = "${hexcode}" onclick = "CopyText(this, this.id)">${hexcode}</span>`;
 
         main.appendChild(ele);
     }
+}
+
+function CopyText(ele, hex) {
+    console.log(hex);
+
+    navigator.clipboard.writeText(hex).then(() => {
+        ele.innerHTML = "Copied..";
+        setTimeout(() => { ele.innerHTML = hex }, 1000);
+    })
 }
